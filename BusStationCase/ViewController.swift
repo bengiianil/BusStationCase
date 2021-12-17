6f6a9496c60d4d560ca0510a8083f69e2643f30f
Bengi Anıl <bengianil34@gmail.com>
2021-12-17 12:26:09 +0300

    Initial Commit

---
+++ BusStationCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusStationCase
+//
+//  Created by Bengi Anıl on 17.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
